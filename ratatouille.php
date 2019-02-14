5ceb8ed542e8a5f76397353da119945dcced2556
Yassine Kharchaf <yassine.kharchaf@gmail.com>
2019-02-14 19:20:52 +0100

    ajout d'un fichier bootstrap pour lancer l'application et faire en sorte que cela ressemble le plus à laravel

--- ratatouille.php
+++ ratatouille.php
@@ -27,19 +27,8 @@ use App\Features\MetaBoxes\RecipeDetailsMetabox;
  *
  */
 
-// Import du autoload.php pour récuperer les class automatiquement sans devoir un require
+ // Import du autoload.php pour récuperer les class automatiquement sans devoir un require
 require_once('autoload.php');
-// Ajout du fichier env.php pour les constantes global
-require_once('env.php');
-// Ajout du fichier helpers.php pour disposé des fonctions helper
-require_once('helpers.php');
-// Ajout d'un listener à l'event "init". le listener est la méthode "register" de la class RecipePostType.
-add_action('init', [RecipePostType::class, 'register']);
-// Ajout d'un listener à l'event "init" pour enregistrer une taxonomy
-add_action('init', [RecipeTaxonomy::class, 'register']);
-// Ajout d'une Metabox pour le postType recipe
-add_action('add_meta_boxes_recipe', [RecipeDetailsMetabox::class, 'add_meta_box']);
-// Ajout d'une action de sauvegarde lors de la sauvegarde d'un post type recipe
-add_action('save_post_' . RecipePostType::$slug, [RecipeDetailsMetabox::class, 'save']);
-// Ajout d'une action pour supprimé toutes les metas d'un post lorsque ce post est supprimé
-add_action('delete_post', 'delete_post_metas');
+
+ //lancement de l'application
+require_one('bootstrap.php');
