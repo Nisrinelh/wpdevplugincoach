c10889a803a098cdec4726efc0970a229933c3f5
Yassine Kharchaf <yassine.kharchaf@gmail.com>
2019-02-15 11:50:56 +0100

    Ajout d'une class TaxonomyRegister qui va prendre en charge d'enregistrer tous les taxonomies

--- config/hooks.php
+++ config/hooks.php
@@ -1,8 +1,8 @@
 <?php
 use App\Features\PostTypes\PostTypeRegister;
 use App\Features\PostTypes\RecipePostType;
-use App\Features\Taxonomies\RecipeTaxonomy;
 use App\Features\MetaBoxes\RecipeDetailsMetabox;
+use App\Features\Taxonomies\TaxonomyRegister;
 
 return [
 /**
@@ -12,7 +12,7 @@ return [
     // Ajout d'un listener à l'event "init". le listener est la méthode "register" de la class RecipePostType.
     ['init', [PostTypeRegister::class, 'register']],
     //Ajout d'un listener à l'event "init" pour enregistrer une taxonomy
-    ['init', [RecipeTaxonomy::class, 'register']],
+    ['init', [TaxonomyRegister::class, 'register']],
     // Ajout d'une Metabox pour le postType recipe
     ['add_meta_boxes_recipe', [RecipeDetailsMetabox::class, 'add_meta_box']],
     // Ajout d'une action de sauvegarde lors de la sauvegarde d'un post type recipe
