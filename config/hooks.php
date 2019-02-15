79a74e4455b360e290d3c535df578afdd0029e53
Yassine Kharchaf <yassine.kharchaf@gmail.com>
2019-02-15 11:46:05 +0100

    Ajout d'une class PostTypeRegister qui va prendre en charge d'enregister tout les types de contenu. l'objectif est de n'avoir qu'un seul event init pour enregistrer tous les types de contenu au lieu d'en avoir un par type de contenu

--- config/hooks.php
+++ config/hooks.php
@@ -1,12 +1,16 @@
 <?php
+use App\Features\PostTypes\PostTypeRegister;
 use App\Features\PostTypes\RecipePostType;
 use App\Features\Taxonomies\RecipeTaxonomy;
 use App\Features\MetaBoxes\RecipeDetailsMetabox;
 
 return [
+/**
+ * Ajout des hooks action
+ */
   'actions' => [
     // Ajout d'un listener à l'event "init". le listener est la méthode "register" de la class RecipePostType.
-    ['init', [RecipePostType::class, 'register']],
+    ['init', [PostTypeRegister::class, 'register']],
     //Ajout d'un listener à l'event "init" pour enregistrer une taxonomy
     ['init', [RecipeTaxonomy::class, 'register']],
     // Ajout d'une Metabox pour le postType recipe
@@ -16,5 +20,11 @@ return [
     // Ajout d'une action pour supprimé toutes les metas d'un post lorsque ce post est supprimé
     ['delete_post', 'delete_post_metas'],
   ],
+
+
+
+  /**
+   * Ajout des hooks filtre
+   */
   'filters' => []
 ];
