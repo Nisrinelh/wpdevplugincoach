3ede30c33ce3ecc2e3fac4dc8a5b6d1d38dda745
Yassine Kharchaf <yassine.kharchaf@gmail.com>
2019-03-12 21:32:04 +0100

    changement d'ordre entre la notification et le titre de la page pour éviter le petit saut lors de l'apparition de la page

--- resources/views/pages/send-mail.html.php
+++ resources/views/pages/send-mail.html.php
@@ -1,9 +1,9 @@
 <div class="wrap">
-    <!-- Ici nous ajoutons une partie d'html afin qui prendra en charge les notifications. On met cela dans un fichier à part afin de pouvoir réutiliser les notifications ailleurs -->
-    <?php view('partials/notice'); ?>
-
     <!-- nous utilisons la fonction get_admin_page_title() pour récupérer le titre de la page admin que l'on a défini lors de l'enregistrement -->
     <h1><?= get_admin_page_title(); ?></h1>
+
+    <!-- Ici nous ajoutons une partie d'html afin qui prendra en charge les notifications. On met cela dans un fichier à part afin de pouvoir réutiliser les notifications ailleurs -->
+    <?php view('partials/notice'); ?>
     <p>Ce formulaire vous permet de contacter vos clients pour leur réservation.</p>
     <!-- Vous pouvez trouver la documentation sur comment bien intégrer votre html à cette adresse https://dotorgstyleguide.wordpress.com/outline/forms/ -->
     <form action="<?= get_admin_url() . '/?action=send-mail'; ?>" method="post">
