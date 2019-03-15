c16c7916ab10628229377c331337536e45ba417d
Yassine Kharchaf <yassine.kharchaf@gmail.com>
2019-03-15 10:52:43 +0100

    Ajout d'une fonctionnalité pour sauvegarder les mails dans la base de donnée. Dans ce commit je n'ai pas mis le lien de toutes les nouvelles fonction. Il faut prendre le reflexe d'aller voir dans la documentation lorsque vous voyez une nouvelle fonction. Ou si vous avez le plugin intelephense de survoler la fonction afin de l'analyser

--- App/Http/Controllers/MailController.php
+++ App/Http/Controllers/MailController.php
@@ -30,6 +30,22 @@ class MailController
     $firstname = sanitize_text_field($_POST['firstname']);
     $message = sanitize_textarea_field($_POST['message']);
 
+    // Nous allons également sauvegarder en base de donnée les mails que nous allons envoyer.
+
+    global $wpdb;
+    // Nous utilisons une fonction pour insérer dans la db. https://developer.wordpress.org/reference/classes/wpdb/insert/
+    $wpdb->insert(
+      $wpdb->prefix . 'rat_mail', // premier argument est le nom de la table. c'est la deuxième fois que l'on écrit ce nom. Il serait bon de faire un refactoring et d'utiliser une constance à la place. Nous le ferons plus tard.
+      [ // Deuxième paramêtre est un tableau dont la clé est le nom de la colonne dans la table et la valeur est la valeur à mettre dans la colonne
+        'userid' => get_current_user_id(),
+        'lastname' => $name,
+        'firstname' => $firstname,
+        'email' => $email,
+        'content' => $message,
+        'created_at' => current_time('mysql')
+      ]
+    );
+
     // la fonction wordpress pour envoyer des mails https://developer.wordpress.org/reference/functions/wp_mail/
     if (wp_mail($email, 'Pour ' . $name . ' ' . $firstname, $message)) {
       $_SESSION['notice'] = [
